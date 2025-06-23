fix: Canonicalise the phase of the eigenvectors in PSD projection to mitigate the ambiguity in the eigenvectors phase during backpropagation - WIP

src/methods_pack/esprit.py
@@ -22,7 +22,7 @@ class ESPRIT(SubspaceMethod):
         upper = signal_subspace[:, :-1]
         lower = signal_subspace[:, 1:]
         phi = torch.linalg.lstsq(upper, lower)[0]  # identical to pinv(A) @ B but faster and stable.
-        eigvalues, _ = torch.linalg.eig(phi)
+        eigvalues = torch.linalg.eigvals(phi)
         eigvals_phase = torch.angle(eigvalues)
         prediction = -1 * torch.arcsin((1 / torch.pi) * eigvals_phase)
 

src/utils.py
@@ -471,9 +471,10 @@ def toeplitz_proj(H: Tensor) -> Tensor:
 
 def psd_proj(T: Tensor) -> Tensor:
     lam, U = torch.linalg.eigh(T)
-    U_fix = U.detach()
+    phase = U[..., 0, :].angle()
+    U = U * torch.exp(-1j * phase)[..., None, :]
     lam_clipped = torch.clamp(lam, min=0.0)
-    return (U_fix * lam_clipped.unsqueeze(-2)) @ U_fix.transpose(-2, -1).conj()
+    return (U * lam_clipped.unsqueeze(-2)) @ U.transpose(-2, -1).conj()
 
 
 def svt(Z: Tensor, tau) -> Tensor: